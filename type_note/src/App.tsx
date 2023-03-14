2f4170d81d5ce3acb1bdc620af2a3f0bd04ea5e8
Sebastian <sebastiansloma@gmail.com>
2023-03-14 12:51:34 +0100

    created notelayout

--- type_note/src/App.tsx
+++ type_note/src/App.tsx
@@ -9,6 +9,7 @@ import { NewNote } from './NewNote';
 import { useLocalStorage } from './useLocalStorage';
 import { v4 as uuidV4 } from 'uuid';
 import { NoteList } from './NoteList';
+import { NoteLayout } from './NoteLayout';
 
 export type Note = {
 	id: string;
@@ -77,8 +78,8 @@ function App() {
 						/>
 					}
 				/>
-				<Route path='/:id'>
-					<Route index element='{<h1>Show</h1>}' />
+				<Route path='/:id' element={<NoteLayout notes={notesWithTags}/>}>
+					<Route index element={<h1>Show</h1>} />
 					<Route path='edit' element='{<h1>Edit</h1>}' />
 				</Route>
 				<Route path='*' element={<Navigate to='/' />} />
